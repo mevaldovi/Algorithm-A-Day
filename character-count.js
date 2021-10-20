b5dd39e7a9fbd7770a11c623fe0b492b20953082
MARIE VALDOVINOS <MEVALDOVI@GMAIL.COM>
2021-10-20 11:06:43 -0500

    added pseudocode

--- character-count.js
+++ character-count.js
@@ -4,27 +4,27 @@
 const testString = "character";
 
 var characterCount = function (str) {
-  if (str.length == 0) {
+  if (str.length == 0) { // if there is no string, throw error
     console.log("Invalid string");
     return;
   }
-//for loop to iterate over string
-for (let i = 0; i < str.length; i++) {
-  //variable counting occurrence
-  let count = 0;//output c = 1, h = 1, a = 2, and so on
+  //for loop to iterate over string
+  for (let i = 0; i < str.length; i++) {
+    //variable counting occurrence
+    let count = 0; //output c = 1, h = 1, a = 2, and so on
 
-  for (let j = 0; j < str.length; j++) {
-    if (str[i] == str[j] && i > j) {
-      break;
+    for (let j = 0; j < str.length; j++) {//loop through it again
+      if (str[i] == str[j] && i > j) {//if one of the strings is longer than the other,
+        // and you need to loop through more than once, exit.
+        break;
+      }
+      if (str[i] == str[j]) {//if the two strings are the same length, add to count.
+        count++;
+      }
     }
-    if (str[i] == str[j]) {
-      count++;
+    if (count > 0) {
+      console.log(`${str[i]} occurs ${count} times`);
     }
   }
-  if (count > 0){
-    console.log(`${str[i]} occurs ${count} times`)
-  };
-}
-}
-
+};
 characterCount(testString);
