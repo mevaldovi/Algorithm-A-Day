35d504a36730918a2eea50a518b29cc4a633a05a
MARIE VALDOVINOS <MEVALDOVI@GMAIL.COM>
2021-10-20 11:00:50 -0500

    character-count worksgit add .

--- character-count.js
+++ character-count.js
@@ -1,20 +1,30 @@
-// Write code to create a function that accepts a string and 
+// Write code to create a function that accepts a string and
 //returns an object containing the number of times each character appears in the string
 
 const testString = "character";
-let results = "";//should output c = 1, h = 1, a = 2, and so on
 
-var characterCount = function(str) {
-    const splitString = testString.split('');
-    console.log(splitString);
-    for (i=0; i < str.length; i++){
-        let characterCount = str[i]
-        let count = 0;
+var characterCount = function (str) {
+  if (str.length == 0) {
+    console.log("Invalid string");
+    return;
+  }
+//for loop to iterate over string
+for (let i = 0; i < str.length; i++) {
+  //variable counting occurrence
+  let count = 0;//output c = 1, h = 1, a = 2, and so on
 
-        results += characterCount//adding characterCount repping how many times a letter occurs, and stores it inside results variable
-        for (j=0; j < str.length; j++){
-            if (characterCount[j] === )
-        }
-
+  for (let j = 0; j < str.length; j++) {
+    if (str[i] == str[j] && i > j) {
+      break;
     }
-};
+    if (str[i] == str[j]) {
+      count++;
+    }
+  }
+  if (count > 0){
+    console.log(`${str[i]} occurs ${count} times`)
+  };
+}
+}
+
+characterCount(testString);
