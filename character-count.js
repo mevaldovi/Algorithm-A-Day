6834b2bec4ef2d4967c645cf9aa2ced696e599c8
MARIE VALDOVINOS <MEVALDOVI@GMAIL.COM>
2021-10-20 10:45:49 -0500

    almost finished

--- character-count.js
+++ character-count.js
@@ -1,3 +1,20 @@
-// Write code to create a function that accepts a string and returns an object containing the number of times each character appears in the string
+// Write code to create a function that accepts a string and 
+//returns an object containing the number of times each character appears in the string
 
-var characterCount = function(str) {};
+const testString = "character";
+let results = "";//should output c = 1, h = 1, a = 2, and so on
+
+var characterCount = function(str) {
+    const splitString = testString.split('');
+    console.log(splitString);
+    for (i=0; i < str.length; i++){
+        let characterCount = str[i]
+        let count = 0;
+
+        results += characterCount//adding characterCount repping how many times a letter occurs, and stores it inside results variable
+        for (j=0; j < str.length; j++){
+            if (characterCount[j] === )
+        }
+
+    }
+};
