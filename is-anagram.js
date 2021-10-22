cde37c1bcc5ca2ec58f333f5957bb8c84f2481eb
MARIE VALDOVINOS <MEVALDOVI@GMAIL.COM>
2021-10-22 11:25:53 -0500

    solved is-anagram.js

--- is-anagram.js
+++ is-anagram.js
@@ -1,3 +1,19 @@
 // Write code to create a function that accepts two strings
 // Return true if the strings are anagrams of each other, otherwise return false
-var isAnagram = function(strA, strB) {};
+const test1 = 'listen';
+const test2 = 'silent';
+const test3 = 'dog';
+const test4 = 'cat';
+
+var isAnagram = function(strA, strB) {
+    const anag1 = strA.split('').sort().join();
+    const anag2 = strB.split('').sort().join();
+    
+    if (anag1 === anag2){
+        console.log('true')
+    } else {
+        console.log('false');
+    }
+};
+
+isAnagram(test1, test2);
