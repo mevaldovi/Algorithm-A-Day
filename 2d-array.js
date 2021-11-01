dad655ba84510f4853553d47dcab43e5ca24726d
MARIE VALDOVINOS <MEVALDOVI@GMAIL.COM>
2021-11-01 14:36:06 -0500

    solved 2d-array.js

--- 2d-array.js
+++ 2d-array.js
@@ -1,12 +1,23 @@
 // Write code to create a function that accepts a two-dimensional array
 // Each 2D element contains either the string "X" or "O" (both capitalized)
 // Return the number of times "X" appears in the 2D array
-const array = ["XOOXXXXOX"];//output: 6
+
+const string = "XO OXX XXOX";//output: 6
 
 const arraySearchForX = function(arr) {
-    const splitArr = arr.split();
-    
+    const splitArr = arr.split(' ');//we're turning string into an array
+    console.log(splitArr);
+    let count = 0;
 
-
+    for (let i = 0; i < arr.length; i++) {
+        const arrIndex = arr[i];
+        
+        if(arrIndex.includes("X")){
+            count++;
+            console.log("Number of X's:" + count)     
+        } else if (!"X"){
+            console.log("nothing to see here");
+        } 
+    }
 };
-arraySearchForX(array);
+arraySearchForX(string);
