2b9e8625bc38e1cb16f77f7b810369ce9d8cf2d8
MARIE VALDOVINOS <MEVALDOVI@GMAIL.COM>
2021-12-02 10:01:10 -0600

    added commentary to simplify-path.js

--- simplify-path.js
+++ simplify-path.js
@@ -1,5 +1,5 @@
 // Write a function that takes in an absolute file path and simplifies it
-const test = "/home/var/./www/../html//sql/";
+const test = "home/var/./www/../html//sql";
 
 function simplify_path(main_path) {
   var parts = main_path.split("/"),
@@ -11,12 +11,17 @@ function simplify_path(main_path) {
     if (part === "." || part === "" || part === "..") {
       if (part === ".." && length > 0) {
         length--;
+        console.log(length)
       }
       continue;
     }
+    //each index in new_path array contains how many elements are in it so far.
+    //if there are any periods or elipses, exclude them
     new_path[length++] = part;
+    console.log(length);
   }
   if (length === 0) {
+    //if there are no variables in the URL path, insert a forward-slash instead
     return "/";
   }
   var result = "";
