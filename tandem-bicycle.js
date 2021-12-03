a9ba715bc91db9a81f533a6212e81ca99c8d2257
MARIE VALDOVINOS <MEVALDOVI@GMAIL.COM>
2021-12-03 11:41:12 -0600

    solved tandem-bicycle.js but still kinda feeling fuzzy about it

--- tandem-bicycle.js
+++ tandem-bicycle.js
@@ -1,28 +1,31 @@
-//
-const redShirtSpeeds = [5, 5, 3, 9, 2];
-const blueShirtSpeeds = [3, 6, 7, 2, 1];
-let maxSpeed = 0;
-const fastest = true;
+let redShirtSpeeds = [5, 5, 3, 9, 2];
+let blueShirtSpeeds = [3, 6, 7, 2, 1];
 
-function tandemBicycle(redShirtSpeeds, blueShirtSpeeds, fastest){
-    let sortedRed = redShirtSpeeds.sort();
-    let sortedBlue = blueShirtSpeeds.sort().reverse();
-
-    //sortedBlue = [7, 6, 3, 2, 1]
-    //sortedRed = [2, 3, 5, 5, 9]
-
-    for(i=0; i < sortedBlue.length; i++){
-        for(j=0; j < sortedRed.length; j++){
-            if(sortedBlue[i] > sortedRed[j]){
-                maxSpeed = sortedBlue[i++];
-            }
-            else if(sortedRed[j] > sortedBlue[i]){
-                maxSpeed = sortedRed[j++]
-            }
-            console.log(maxSpeed)
-
-        }
-
+function tandemBicycle(redShirtSpeeds, blueShirtSpeeds, fastest) {
+  redShirtSpeeds.sort((a, b) => a - b);
+  blueShirtSpeeds.sort((a, b) => a - b);
+  console.log(redShirtSpeeds, blueShirtSpeeds);
+  
+  if (!fastest) {
+    reverseArrayInPlace(redShirtSpeeds);
+    let totalSpeed = 0;
+    for (let i = 0; i < redShirtSpeeds.length; i++) {
+      const rider1 = redShirtSpeeds[i];
+      const rider2 = blueShirtSpeeds[blueShirtSpeeds.length - i - 1];
+      totalSpeed += Math.max(rider1, rider2);
     }
+    console.log(totalSpeed);
+  }
+  function reverseArrayInPlace(array) {
+    let start = 0;
+    let end = array.length - 1;
+    while (start > end) {
+      const temp = array[start];
+      array[start] = array[end];
+      array[end] = temp;
+      start++;
+      end--;
+    }
+  }
 }
-tandemBicycle(redShirtSpeeds, blueShirtSpeeds, fastest);
+tandemBicycle(redShirtSpeeds, blueShirtSpeeds);
