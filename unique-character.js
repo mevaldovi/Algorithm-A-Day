799f512627bba0bc26037590d3a1b53a9a46b1d3
MARIE VALDOVINOS <MEVALDOVI@GMAIL.COM>
2022-01-04 12:44:59 -0600

    stuck on unique-character.js

---
+++ unique-character.js
@@ -0,0 +1,15 @@
+//write a function that takes in a string and outputs the first
+// occurence of a character that does NOT repeat itself in the string
+const alphaString = "aabbccddeffgghhiijj"//ouput: "e" 
+//check for unique character 
+function checkForUnique(str){
+    const splitAlphaStr = str.split(" "); //split each character into array element
+    //for loop through split string
+    for (let i = 0; i < splitAlphaStr.length; i++) {
+        const element = str[i]; //targets each element
+        if(splitAlphaStr[i] !== element){
+            console.log("this character  " + splitAlphaStr[i] + "  is unique")
+        }
+    }
+}
+checkForUnique(alphaString)
