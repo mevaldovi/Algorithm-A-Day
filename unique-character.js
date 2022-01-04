7a0b647e18b63c0eafa2726c996e246ae0949a99
MARIE VALDOVINOS <MEVALDOVI@GMAIL.COM>
2022-01-04 14:00:37 -0600

    as per line 15, its only counting number of characters in string, not unique elemets

--- unique-character.js
+++ unique-character.js
@@ -1,16 +1,17 @@
 //write a function that takes in a string and outputs the first
 // occurence of a character that does NOT repeat itself in the string
-const alphaString = "aabbccddeffgghh"//ouput: "e" 
-//check for unique character 
-function checkForUnique(str){
-    var count = 0; //create count variable
-    const splitAlphaStr = str.split(" "); //split each character into array element
-    //for loop through split string
-    for (let i = 0; i < splitAlphaStr.length; i++) {
-        const element = str[i]; //targets each element
-        if(splitAlphaStr[i] !== element){
-            console.log("this character  " + splitAlphaStr[i] + "  is unique")
-        }
+const alphaString = "aabbccddeffgghh"; //ouput: "e"
+//check for unique character
+function checkForUnique(str) {
+  var count = 0; //create count variable
+  const splitAlphaStr = str.split(" "); //split each character into array element
+  //for loop through split string
+  for (let i = 0; i < str.length; i++) {
+    const element = str[i]; //targets each element
+    if (splitAlphaStr[i] !== str[i]) {
+      count++;
     }
+  }
+  console.log("this character  " + count + "  is unique");
 }
-checkForUnique(alphaString)
+checkForUnique(alphaString);
