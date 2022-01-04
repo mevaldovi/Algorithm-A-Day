dcbe86cdcf9de87521012e55d933f10547395b64
MARIE VALDOVINOS <MEVALDOVI@GMAIL.COM>
2022-01-04 13:48:09 -0600

    algo in unique-character.js does not work properly

--- unique-character.js
+++ unique-character.js
@@ -1,6 +1,6 @@
 //write a function that takes in a string and outputs the first
 // occurence of a character that does NOT repeat itself in the string
-const alphaString = "aabbccddeffgghhiijj"//ouput: "e" 
+const alphaString = "aabbccddeffgghh"//ouput: "e" 
 //check for unique character 
 function checkForUnique(str){
     const splitAlphaStr = str.split(" "); //split each character into array element
