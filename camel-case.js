a9006c78e5e5c63bd13b120940c116d9c893c895
Marie Valdovinos <marie.valdovinos@amtil.co>
2022-04-21 00:06:04 -0500

    got pretty darn close. time to turn in. More updates later

--- camel-case.js
+++ camel-case.js
@@ -1,5 +1,20 @@
 // Write code to create a function that accepts a string and returns the string in camelCase
+const stringtoCamelCase = "text That I WaNt to make cAMEL case";
+const makeCamelCase = (str) => {
+    let substringChange =
+    str
+    .split(" ")//split at each space between each word
+    .map((e, i) => //create a new array with the reflective changes from toUpperCase && toLowerCase methods
+      i ? e.charAt(0).toUpperCase() + e.slice(1).toLowerCase() : e.toLowerCase()
+      //translation: if a character exists at its index position, take the first character at index[0]
+      //and concat that to the rest of the substring  that is lowercase; otherwise, leave lowercase
+    )
+    .join("");//join each word back together
+    console.log(substringChange)
+}
+makeCamelCase(stringtoCamelCase);
+// map() creates a NEW array from calling a function (or in this case, a method) for every array element.
 
-var camelCase = function(str) {
-    
-};
+// map() calls a function once for each element in an array.
+
+// map() does NOT change the original array.
--- toUpperCase.js
+++ toUpperCase.js
@@ -4,9 +4,6 @@ const string = "a man a plan a canal panama";
 const string2 = "hello";
 
 const upperCase = function (str) {
-  //   const first = str.toUpperCase(); // output: 'A'
-  //   console.log(first);
-
   const splitStr = str.split(" ");
   console.log(splitStr);
 
@@ -14,9 +11,8 @@ const upperCase = function (str) {
 
   for (i = 0; i < splitStr.length; i++) {
     //['a', 'man', 'a', 'plan', 'a', 'canal', 'panama']
-    // if (splitStr[i]===)
     let word = splitStr[i];
-    word[0] = word[0].toUpperCase();
+    word[0] = word[0].toUpperCase(); //converting first character of each word to uppercase letter
     console.log(word[0].toUpperCase());
 
     const splitWord = word.split("");
@@ -24,20 +20,13 @@ const upperCase = function (str) {
 
     splitWord[0] = splitWord[0].toUpperCase();
     console.log(splitWord[0].toUpperCase());
-    
-    splitWord.splice(0, 1, splitWord[0].toUpperCase())
-    console.log(splitWord.join(''));
 
-    array.push(splitWord.join(''));
+    splitWord.splice(0, 1, splitWord[0].toUpperCase());
+    console.log(splitWord.join(""));
+
+    array.push(splitWord.join(""));
     console.log(array);
-
-
-    // const smoosh = word[0].join(word[0].toUpperCase()); //exxpected output: "Hello"
-    // console.log(smoosh);
-
-    // const replaceLetter = splitStr.replace(splitStr[0], first);
-    // console.log(replaceLetter);
   }
-  console.log(array.join(' '));
+  console.log(array.join(" "));
 };
 upperCase(string);
