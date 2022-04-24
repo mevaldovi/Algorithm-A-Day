56bb421e90044edf05b5f99b7b7fd0bbbe59bb66
Marie Valdovinos <marie.valdovinos@amtil.co>
2022-04-24 14:45:44 -0500

    addded pseudo for newfile.js

--- new-file.js
+++ new-file.js
@@ -5,16 +5,22 @@
 // of this string, e.g. "GOD," "OGD," "DOG," "GDO," "ODG," and "DGO." ----------***
 const string = "GOD";
 
-function printAll(str) {
-  const strToArr = str.split(""); //convert string to array -> ["G", "O", "D"]
-  //loop through each element in array
-  for (i = 0; i < str.length; i++) {
-    //find some way to copy strToArr reflecting all possible permutations
-    const newArray = strToArr.map(newFunction)
-    //newFunction should return all possible permutations
-  }
-  function newFunction(num){ //num represents number of permutations: 6
-      //find some way to return newArray that has already printed all 6 permutations
-  }
-}
-printAll(string);
+//STEP ONE: TAKE THE STRING "GOD" AND ITERATE THROUGH ONE LETTER AT A TIME
+//STEP TWO: ITERATE THROUGH THE FIRST CHARACTER IN STRING
+//STEP THREE: ITERATE THROUGH THE REMAINING TWO CHARACTERS IN STRING, 
+//STEP FOUR: 
+//STEP FIVE:
+
+// function printAll(str) {
+//   const strToArr = str.split(""); //convert string to array -> ["G", "O", "D"]
+//   //loop through each element in array
+//   for (i = 0; i < str.length; i++) {
+//     //find some way to copy strToArr reflecting all possible permutations
+//     const newArray = strToArr.map(newFunction)
+//     //newFunction should return all possible permutations
+//   }
+//   function newFunction(num){ //num represents number of permutations: 6
+//       //find some way to return newArray that has already printed all 6 permutations
+//   }
+// }
+// printAll(string);
