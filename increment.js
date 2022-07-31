898b05ed2dedfcffabccf5d353ed3c1b665b5c2e
Marie Valdovinos <marie.valdovinos@amtil.co>
2022-07-31 18:39:04 -0500

    started increment.js

--- increment.js
+++ increment.js
@@ -1,5 +1,7 @@
 // Write a function to take an array of integers and return the minimum number of 
 //increments required across the array to ensure that every number is unique
 const array = [0,1,2,3,4,4]
-var minIncrement = function(nums) {};
+var minIncrement = function(nums) {
+    return array[i]
+};
 minIncrement(array);
