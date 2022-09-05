10fd4fd1dd4c135200d84b5bcd769a69d615dd61
Marie Valdovinos <marie.valdovinos@amtil.co>
2022-09-04 19:04:37 -0500

    sortedSquaresrray

--- sortedSqaresArray.js
+++ sortedSqaresArray.js
@@ -1,5 +1,27 @@
 // Sorted Squared Array from Algoexpert.com
 
+//Given an array of integers that are sorted in increasing order, 
+//write a function that squares all the integers in the array and returns them in a new array, 
+//also sorted in increasing order.
+
+function sortedSquaredArray(array) {
+    const sortedSquares = new Array(array.length).fill(0);
+    let smallerValueIdx = 0; 
+    let largerValueIdx = array.length - 1; //reps the last number in the index of the array
+  
+    for (let idx = array.length - 1; idx >= 0; idx--) {
+      const smallerValue = array[smallerValueIdx];
+      const largerValue = array[largerValueIdx];
+      if (Math.abs(smallerValue) >= Math.abs(largerValue)) {
+        sortedSquares[idx] = smallerValue * smallerValue;
+        smallerValueIdx++;
+      } else {
+        sortedSquares[idx] = largerValue * largerValue;
+        largerValueIdx--;
+      }
+    }
+    return sortedSquares;
+  }
 
 
 
